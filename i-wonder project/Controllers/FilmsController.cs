3e8e2604549561d921bed005303b7d34e4ba42d1
imanjalee <imanjalee99@outlook.com>
2021-05-06 21:07:45 +0530

    Second

---
+++ i-wonder project/Controllers/FilmsController.cs
@@ -0,0 +1,30 @@
+﻿using i_wonder_project.OtherClasses;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace i_wonder_project.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("[controller]/[action]")]
+    public class FilmsController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public FilmsController(IHttpClientFactory clientFactory)
+        {
+            _httpClientFactory = clientFactory;
+        }
+
+        [HttpGet]
+        public List<Films> GetFilms()
+        {
+            PublicAPiConnectors connector = new PublicAPiConnectors();
+            List<Films> filmsList = connector.GetFilmDetails(_httpClientFactory);
+            return filmsList;
+        }
+    }
+}
---
+++ i-wonder project/OtherClasses/PublicAPiConnectors.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace i_wonder_project.OtherClasses
+{
+    public class PublicAPiConnectors
+    {
+        public List<Films> GetFilmDetails(IHttpClientFactory _httpClientFactory)
+        {
+
+            string[] imdbUniqueIdentifiers = new string[10];
+            imdbUniqueIdentifiers[0] = "tt0407887";
+            imdbUniqueIdentifiers[1] = "tt0068646";
+            imdbUniqueIdentifiers[2] = "tt0468569";
+            imdbUniqueIdentifiers[3] = "tt0050083";
+            imdbUniqueIdentifiers[4] = "tt0108052";
+            imdbUniqueIdentifiers[5] = "tt0167260";
+            imdbUniqueIdentifiers[6] = "tt0110912";
+            imdbUniqueIdentifiers[7] = "tt0060196";
+            imdbUniqueIdentifiers[8] = "tt1375666";
+            imdbUniqueIdentifiers[9] = "tt0133093";
+
+            List<Films> films = new List<Films>();
+
+            foreach (string imdbIdentifier in imdbUniqueIdentifiers)
+            {
+                Films film = OnGet(_httpClientFactory, imdbIdentifier).Result;
+                films.Add(film);
+            }
+            return films;
+        }
+    }
+    public async Task<Films> OnGet(IHttpClientFactory _clientFactory, string filmID)
+    {
+        Films film = new Films();
+        var request = new HttpRequestMessage(HttpMethod.Get, $"http://www.omdbapi.com/?i={filmID}&apikey=1e8fa3a");
+        var client = _clientFactory.CreateClient();
+        var response = await client.SendAsync(request);
+        if (response.IsSuccessStatusCode)
+        {
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            film = await JsonSerializer.DeserializeAsync
+                <Films>(responseStream);
+        }
+        return film;
+    }
+}
